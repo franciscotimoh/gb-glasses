9b4f3802e707af3598824eff4a123a4f2b708695
DonChenn <132190496+DonChenn@users.noreply.github.com>
2023-05-22 01:15:43 -0700

    Update glasses.py
    doc strings

--- src/glasses.py
+++ src/glasses.py
@@ -1,3 +1,11 @@
+"""
+This module uses a pre-trained image recognition model to detect a walk sign in a live camera feed.
+
+This module assumes the existence of a custom pre-trained YOLOv5 model file named 'model_v9.pt' in the current
+directory. This module was mainly provided by Mr. Jeffrey Truong
+"""
+
+
 # For changes to be put into effect during start up, run:
 # sudo systemctl daemon-reload
 
@@ -5,19 +13,27 @@ import cv2
 import torch
 import numpy as np
 import time
-
 from sensor import Sensor
 
 
 def camera():
+    """Detects a walk sign in a live camera feed and returns the average confidence score of the detected walk signs.
+
+    Returns:
+        average_prob (float): The average confidence score of the detected walk signs.
+    """
+
     image_probability = []
 
+    # Loads the pre-trained image recognition model.
     model = torch.hub.load('ultralytics/yolov5', 'custom', path='model_v9.pt', trust_repo=True)
 
+    # Initializes the camera and sets the frame dimensions.
     cap = cv2.VideoCapture(0)
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 180)
 
+    # Set GPIO for Jetson Nano
     gpio_pin = 18
     sensor = Sensor(gpio_pin)
 
@@ -25,6 +41,7 @@ def camera():
 
     walk_count = 0
 
+    # Enters a loop to continuously read frames from the camera feed.
     while True:
         ret, frame = cap.read()
 
@@ -42,9 +59,13 @@ def camera():
             if len(classes) == 0:
                 sensor.set_low()
                 print(" === no detections")
+
+            # Performs object detection on each frame using the loaded model
             for obj in results.pandas().xyxy[0].values:
                 if obj[5] == 0:
                     print(f'Don-t Walk : {obj[4]:.2f}')
+
+                # If a walk sign is detected, updates the walk count and appends the confidence score to a list.
                 elif obj[5] == 1:
                     walk_count += 2
                     print(f'Walk : {obj[4]:.2f}')
@@ -52,6 +73,7 @@ def camera():
 
             if walk_count > 0: walk_count -= 1
 
+            # If the walk count reaches a threshold, activates the walk sign using the sensor.
             if walk_count == 10:
                 sensor.set_high()
                 time.sleep(1)
@@ -63,6 +85,7 @@ def camera():
 
             time.sleep(0.1)
 
+        # Turns off camera
         if (cv2.waitKey(1) & 0xFF) == ord("q"):
             print('break')
             cap.release()
@@ -70,6 +93,7 @@ def camera():
             cv2.destroyAllWindows()
             break
 
+    # calculates the average confidence score for the detected walk signs.
     average_prob = sum(image_probability) / len(image_probability)
 
     return average_prob
