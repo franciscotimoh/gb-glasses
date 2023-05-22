efb6018a887bb41c353f99f27c99bb5c2dfadbb2
DonChenn <132190496+DonChenn@users.noreply.github.com>
2023-05-22 01:16:58 -0700

    Update record.py
    added doc strings

--- src/record.py
+++ src/record.py
@@ -1,27 +1,40 @@
+"""Records and saves an audio file to calculate the decibels.
+
+This module records audio in a .wav file and records the average decibels each second in three second intervals.
+"""
+
 import pyaudio
 import wave
 import audioop
 from math import log10
-import time
 
 
 def calc_decibel():
-    time.sleep(3)
-    CHUNK = 1024  # Record in chunks of 1024 samples
+    """ Function to record and calculate decibels.
+
+    Returns:
+        average decibels in three seconds
+
+    """
+    
+    # settings to record .wav file: 1024 samples, 16 bits per sample, 44100 sampls per second
+    CHUNK = 1024
     total_decibel_list = []
     one_sec_decibel = []
     two_sec_decibel = []
     three_sec_decibel = []
-    sample_format = pyaudio.paInt16  # 16 bits per sample
+    sample_format = pyaudio.paInt16
     channels = 2
-    fs = 44100  # Record at 44100 samples per second
+    fs = 44100
     interval = 1
     filename = "output.wav"
 
-    p = pyaudio.PyAudio()  # Create an interface to PortAudio
+    # Create an interface to PortAudio
+    p = pyaudio.PyAudio()
 
     print('Recording for decibels...')
 
+    # Starts recording
     stream = p.open(format=sample_format,
                     channels=channels,
                     rate=fs,
@@ -58,6 +71,7 @@ def calc_decibel():
     # Stop and close the stream
     stream.stop_stream()
     stream.close()
+
     # Terminate the PortAudio interface
     p.terminate()
 
@@ -71,16 +85,18 @@ def calc_decibel():
     wf.writeframes(b''.join(frames))
     wf.close()
 
+    # Finds the average decibels from 0 to 1 seconds, 1 to 2 seconds, and 2 to 3 seconds, and across total 3 seconds
     first_average = sum(one_sec_decibel) / len(one_sec_decibel)
     second_average = sum(two_sec_decibel) / len(two_sec_decibel)
     third_average = sum(three_sec_decibel) / len(three_sec_decibel)
     average_decibel = (first_average + second_average + third_average) / (len(one_sec_decibel) + len(two_sec_decibel) + len(three_sec_decibel))
 
+    # Compares the average decibels to determine if a car is coming towards you or not or neither
     if first_average < second_average < third_average:
         print('\nGetting louder. Car may be moving towards you.\n')
     elif first_average > second_average > third_average:
         print('\nGetting quieter. Car may be moving away from you.\n')
     else:
-        print('Not getting louder or quieter.')
+        print('\nNot getting louder or quieter.\n')
 
-    return calc_decibel()
+    return average_decibel
