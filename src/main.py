77a45d00c5fc186e17e44c801d29be9aa58b364c
DonChenn <132190496+DonChenn@users.noreply.github.com>
2023-05-22 12:30:13 -0700

    Update main.py
    added speech to text certainty with image certainty
    added docstrings

--- src/main.py
+++ src/main.py
@@ -1,26 +1,51 @@
+"""The main module for running the accessibility glasses.
+
+Synchronously runs the camera, ultrasonic sensor, and microphone modules all at the same time.
+"""
+
 from glasses import camera
 from record import calc_decibel
 from speech_to_text import speech_to_text
-# from ultra_sonic import get_distance
-
-from multiprocessing import Process
+from ultra_sonic import get_distance
+import multiprocessing
 
 
 def main():
+    """Main function for starting and running all the sensors' modules"""
+
+    # Gets the certainty of a walk sign
     image_probability = camera()
 
-    # ultra_sonic = Process(target=get_distance)
-    stt = Process(target=speech_to_text)
-    db = Process(target=calc_decibel)
+    while True:
+        # Speech to Text certainty
+        queue = multiprocessing.Queue()
+        stt = multiprocessing.Process(target=speech_to_text, args=(queue,))
+        stt.start()
+        stt.join()
+        result = queue.get()
+        print("Increased certainty due to hearing:", result)
 
-    # ultra_sonic.start()
-    stt.start()
-    db.start()
+        # Calls on each sensor in their own process
+        ultra_sonic = multiprocessing.Process(target=get_distance)
+        db = multiprocessing.Process(target=calc_decibel)
 
-    # ultra_sonic.join()
-    stt.join()
-    db.join()
+        # Start the process’s activity.
+        ultra_sonic.start()
+        stt.start()
+        db.start()
+
+        # Method blocks until each process that is called terminates
+        ultra_sonic.join()
+        stt.join()
+        db.join()
+
+        if result is not None:
+            image_probability = image_probability * 1.25
+            if image_probability > 1:
+                image_probability = 1
+            print(image_probability)
 
 
 if __name__ == '__main__':
     main()
+
