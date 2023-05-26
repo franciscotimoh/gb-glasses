1521987ac1668292934547b7ddbfb010610a63d9
DonChenn <132190496+DonChenn@users.noreply.github.com>
2023-05-26 01:30:56 -0700

    Update main.py with camera

--- src/main.py
+++ src/main.py
@@ -3,47 +3,68 @@
 Synchronously runs the camera, ultrasonic sensor, and microphone modules all at the same time.
 """
 
-from glasses import camera
 from record import calc_decibel
 from speech_to_text import speech_to_text
-from ultra_sonic import get_distance
+# from ultra_sonic import get_distance
 import multiprocessing
+from detect import mayne, parse_opt
 
+
+def camera():
+    opt = parse_opt()
+    mayne(opt)
 
 def main():
     """Main function for starting and running all the sensors' modules"""
 
     # Gets the certainty of a walk sign
-    image_probability = camera()
+
+    image_probability = 0.7
 
     while True:
-        # Speech to Text certainty
+        # SPEECH TO TEXT
+        # Starts the process of speech to text
         queue = multiprocessing.Queue()
-        stt = multiprocessing.Process(target=speech_to_text, args=(queue,))
-        stt.start()
-        stt.join()
-        result = queue.get()
-        print("Increased certainty due to hearing:", result)
+
 
         # Calls on each sensor in their own process
-        ultra_sonic = multiprocessing.Process(target=get_distance)
+        # ultra_sonic = multiprocessing.Process(target=get_distance)
+        stt = multiprocessing.Process(target=speech_to_text, args=(queue,))
         db = multiprocessing.Process(target=calc_decibel)
+        cam = multiprocessing.Process(target=camera)
+
 
         # Start the process’s activity.
-        ultra_sonic.start()
-        stt.start()
+        # ultra_sonic.start()
         db.start()
+        cam.start()
+        stt.start()
 
         # Method blocks until each process that is called terminates
-        ultra_sonic.join()
-        stt.join()
-        db.join()
+        # ultra_sonic.join()
+        # cam.join()
+        # db.join()
+        # stt.join()
 
-        if result is not None:
+        # Gets the result of what was heard
+        result = queue.get()
+        print("Received result:", result)
+
+        # Determines the new certainty to cross the street
+        if 'walk sign' in result:
             image_probability = image_probability * 1.25
             if image_probability > 1:
                 image_probability = 1
-            print(image_probability)
+        elif 'wait' in result:
+            image_probability = image_probability - (image_probability * 0.25)
+            if image_probability < 0:
+                image_probability = 0
+        print(image_probability)
+
+        if image_probability > 0.75:
+            print('YOU CAN WALK')
+        else:
+            print('DO NOT WALK')
 
 
 if __name__ == '__main__':
