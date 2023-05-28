fae4bd747b4f2f23a4eed9ff7d0319bbdf3e9960
DonChenn <132190496+DonChenn@users.noreply.github.com>
2023-05-27 23:47:07 -0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -7,32 +7,23 @@ from record import calc_decibel
 from speech_to_text import speech_to_text
 # from ultra_sonic import get_distance
 import multiprocessing
-from detect import mayne, parse_opt
+from glasses_cam import camera
 
-
-def camera():
-    opt = parse_opt()
-    mayne(opt)
 
 def main():
     """Main function for starting and running all the sensors' modules"""
 
-    # Gets the certainty of a walk sign
-
-    image_probability = 0.7
-
     while True:
         # SPEECH TO TEXT
         # Starts the process of speech to text
-        queue = multiprocessing.Queue()
-
+        speech = multiprocessing.Queue()
+        certainty = multiprocessing.Queue()
 
         # Calls on each sensor in their own process
         # ultra_sonic = multiprocessing.Process(target=get_distance)
-        stt = multiprocessing.Process(target=speech_to_text, args=(queue,))
+        stt = multiprocessing.Process(target=speech_to_text, args=(speech,))
         db = multiprocessing.Process(target=calc_decibel)
-        cam = multiprocessing.Process(target=camera)
-
+        cam = multiprocessing.Process(target=camera, args=(certainty,))
 
         # Start the process’s activity.
         # ultra_sonic.start()
@@ -42,31 +33,32 @@ def main():
 
         # Method blocks until each process that is called terminates
         # ultra_sonic.join()
-        # cam.join()
-        # db.join()
-        # stt.join()
+        cam.join()
+        db.join()
+        stt.join()
 
         # Gets the result of what was heard
-        result = queue.get()
-        print("Received result:", result)
+        text = speech.get()
+        image_probability = float(certainty.get())
+        print(image_probability)
+        print("Received result:", text)
 
         # Determines the new certainty to cross the street
-        if 'walk sign' in result:
+        if 'walk sign is on' in text:
             image_probability = image_probability * 1.25
             if image_probability > 1:
                 image_probability = 1
-        elif 'wait' in result:
+        elif 'wait' in text:
             image_probability = image_probability - (image_probability * 0.25)
             if image_probability < 0:
                 image_probability = 0
         print(image_probability)
 
         if image_probability > 0.75:
-            print('YOU CAN WALK')
+            print('YOU CAN WALK HEHEHEHEH')
         else:
-            print('DO NOT WALK')
+            print('DO NOT WALK AHHHHH')
 
 
 if __name__ == '__main__':
     main()
-
